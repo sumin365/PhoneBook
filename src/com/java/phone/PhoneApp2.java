2e0c5edd5b549c30c008321071e15e6ff0b70261
송수민 <songsumin77@naver.com>
2021-03-23 14:32:42 +0900

    3

---
+++ src/com/java/phone/PhoneApp2.java
@@ -0,0 +1,8 @@
+package com.java.phone;
+
+public class PhoneApp2 {
+	public static void main(String[] args) {
+		PhoneController pController = new PhoneController();
+		pController.excute();
+	}
+}
